1022eb7b69f0aaa9def30e737e07582e1374495f
LittringerJulian <56301487+LittringerJulian@users.noreply.github.com>
2022-04-14 19:37:08 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow, Menu, Tray, nativeImage } = require('electron');
+const path = require('path');
+const nircmd = require('nircmd');
+
+if (require('electron-squirrel-startup')) {
+    app.quit();
+}
+
+let tray = null
+app.whenReady().then(() => {
+    // tray icon
+    var iconPath = path.join(__dirname, '/assets/sound_settings_icon.png')
+    let trayIcon = nativeImage.createFromPath(iconPath);
+    tray = new Tray(trayIcon)
+
+    const contextMenu = Menu.buildFromTemplate([
+        { label: 'Sony AUX + Rode', click: () => switchAudioDevices("Headphones Sony AUX", "Microphone RODE NT-USB") },
+        { label: 'Sony BT + Rode', click: () => switchAudioDevices("Headphones Sony BT", "Microphone RODE NT-USB") },
+        { label: 'Soundbar + Rode', click: () => switchAudioDevices("Soundbar Samsung", "Microphone RODE NT-USB") },
+        { label: 'Oculus Quest 2', click: () => switchAudioDevices("Headphones Oculus", "Microphone Oculus") },
+        { label: 'Quit', click: app.quit }
+    ])
+    tray.setContextMenu(contextMenu)
+})
+
+
+function switchAudioDevices(speaker, mic) {
+    nircmd(`nircmd setdefaultsounddevice "${speaker}" 1`);
+    nircmd(`nircmd setdefaultsounddevice "${speaker}" 2`);
+
+    nircmd(`nircmd setdefaultsounddevice "${mic}" 1`);
+    nircmd(`nircmd setdefaultsounddevice "${mic}" 2`);
+}
